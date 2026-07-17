e7f0c120b535ebf3ff5c1c4e6f76314583804bbf
RAKSHITHA-SELVAKUMAR <rakshithask2007@gmail.com>
2026-07-17 15:15:32 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,10 @@
+// Last updated: 7/17/2026, 3:15:31 PM
+class Solution {
+    public boolean canWinNim(int n) {
+        
+        if (n % 4 != 0)
+            return true;
+        else
+            return false;
+    }
+}
